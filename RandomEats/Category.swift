9e621baea814634d59263b835a1bd289567baad8
MissDroid <memissdroid@gmail.com>
2016-04-25 15:39:49 -0400

    all classes

---
+++ RandomEats/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  RandomEats
+//
+//  Created by Maliha Fairooz on 4/25/16.
+//  Copyright © 2016 RandomEats. All rights reserved.
+//
+
+import UIKit
+
+class Category: NSObject {
+    
+    //blah
+
+}
---
+++ RandomEats/ResultsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ResultsCollectionViewCell.swift
+//  RandomEats
+//
+//  Created by Maliha Fairooz on 4/25/16.
+//  Copyright © 2016 RandomEats. All rights reserved.
+//
+
+import UIKit
+
+class ResultsCollectionViewCell: UICollectionViewCell {
+    
+}
